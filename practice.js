210a9ebef0f366d02b9f198e053fc75a870df4cf
Ryoma Aoyama <ryoma-baseball@hotmail.co.jp>
2015-09-10 21:14:38 +0900

    Edit practice.js

--- practice.js
+++ practice.js
@@ -2,6 +2,18 @@ Number.prototype.plus = function ( num ) {
   return this + num;
 };
 
+Number.prototype.multiple = function ( num ) {
+  return this * num;
+};
+
+Number.prototype.divide = function ( num ) {
+  return this / num;
+};
+
 var number = 100;
 var result = number.plus(1000);
+var result2 = number.multiple(3);
+var result3 = number.divide(4);
 console.log(result);
+console.log(result2);
+console.log(result3);
