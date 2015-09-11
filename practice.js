5dd8cb6af641dd7e6a6ee8e4aa3f1a3e6f986526
ZennoFact <zenno04.g@gmail.com>
2015-09-11 10:04:20 +0900

    [Add] function mod to practice.js

--- practice.js
+++ practice.js
@@ -4,9 +4,14 @@ Number.prototype.plus = function ( num ) {
 Number.prototype.minus = function ( num ) {
   return this - num;
 };
+Number.prototype.mod = function ( num ) {
+  return this % num;
+}
 
 var number = 100;
 var result1 = number.plus(1000);
 var result2 = number.minus(10);
+var result3 = number.mod(2);
 console.log(result1);
 console.log(result2);
+console.log(result3);
